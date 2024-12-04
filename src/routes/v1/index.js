a6f1ca581d1b47d229c4f23950e4f57cd1ef56d8
Vittor Fonseca Serra <vittorserradev@gmail.com>
2024-12-04 12:21:12 -0300

    update: Fiz a rota que cria o novo usuário

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -2,7 +2,9 @@ import { Router } from "express";
 const router = Router();
 
 import Home from "./homeRoute.js";
+import User from "./userRoute.js";
 
 router.use("/home", Home);
+router.use("/user", User);
 
 export default router;
